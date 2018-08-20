f8c659ebee229b1a77f923e7e8d78ac018671216
Maicon Dias Castro <maicondcastro@teiasoftware.com>
2018-08-20 19:50:54 -0300

    Criação de layout de busca de cidades

---
+++ app/src/main/java/com/stefanini/cidadeclima/activities/BuscaActivity.java
@@ -0,0 +1,15 @@
+package com.stefanini.cidadeclima.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stefanini.cidadeclima.R;
+
+public class BuscaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busca);
+    }
+}
