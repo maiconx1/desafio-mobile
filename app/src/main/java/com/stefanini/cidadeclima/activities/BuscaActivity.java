1e10b03f6c5874167b2197de0206d51dbb92f802
Maicon Dias Castro <maicondcastro@teiasoftware.com>
2018-08-21 00:23:50 -0300

    Atualização busca

--- app/src/main/java/com/stefanini/cidadeclima/activities/BuscaActivity.java
+++ app/src/main/java/com/stefanini/cidadeclima/activities/BuscaActivity.java
@@ -32,7 +32,6 @@ public class BuscaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busca);
-        Singleton.getInstance().mockCidades();
         holder = new BuscaActivity.Holder();
 
         ActionBar actionBar = getSupportActionBar();
