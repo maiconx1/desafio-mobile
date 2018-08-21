52de448b377443cf56bc68956c11fc991ffb2cc5
Maicon Dias Castro <maicondcastro@teiasoftware.com>
2018-08-21 20:36:21 -0300

    Detalhes concluídos

--- app/src/main/java/com/stefanini/cidadeclima/activities/FavoritosActivity.java
+++ app/src/main/java/com/stefanini/cidadeclima/activities/FavoritosActivity.java
@@ -1,6 +1,5 @@
 package com.stefanini.cidadeclima.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,13 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.stefanini.cidadeclima.R;
 import com.stefanini.cidadeclima.adapters.AdapterFavoritos;
 import com.stefanini.cidadeclima.classes.Favorito;
-import com.stefanini.cidadeclima.classes.OpenWeatherJson;
+import com.stefanini.cidadeclima.classes.ListaOpenWeather;
 import com.stefanini.cidadeclima.classes.Singleton;
 
 import java.io.BufferedReader;
@@ -37,7 +38,7 @@ public class FavoritosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritos);
-        Singleton.getInstance().mockFavoritos();
+        //Singleton.getInstance().mockFavoritos();
         holder = new Holder();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -47,48 +48,62 @@ public class FavoritosActivity extends AppCompatActivity {
         holder.getFab().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
                 Intent intent = new Intent(FavoritosActivity.this, BuscaActivity.class);
                 startActivity(intent);
             }
         });
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(FavoritosActivity.this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         this.favoritos = Singleton.getInstance().getFavoritos();
-        adapter = new AdapterFavoritos(favoritos, FavoritosActivity.this);
-        holder.getLstFavoritos().setAdapter(adapter);
-        holder.getLstFavoritos().setLayoutManager(linearLayoutManager);
-        holder.getSwpFavoritos().setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                atualizaLista();
-            }
-        });
-        atualizaLista();
     }
 
     private void atualizaLista() {
-        adapter.setFavoritos(favoritos);
-        holder.getSwpFavoritos().setRefreshing(false);
+        holder.getSwpFavoritos().setRefreshing(true);
+        holder.getLstFavoritos().setVisibility(View.GONE);
+        ClimaAssincrono assincrono = new ClimaAssincrono();
+        StringBuilder ids = new StringBuilder();
+        for(Favorito fav : favoritos) {
+            ids.append((ids.length() == 0) ? "" : ",").append(fav.getId());
+        }
+        assincrono.execute("https://api.openweathermap.org/data/2.5/group?id=" + ids + "&appid=2bac87e0cb16557bff7d4ebcbaa89d2f&lang=pt&units=metric");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        atualizaLista();
+        if(favoritos.size() > 0) {
+            holder.getTxtVazio().setVisibility(View.GONE);
+            holder.getSwpFavoritos().setVisibility(View.VISIBLE);
+            adapter = new AdapterFavoritos(favoritos, FavoritosActivity.this);
+            holder.getLstFavoritos().setAdapter(adapter);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(FavoritosActivity.this);
+            linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            holder.getLstFavoritos().setLayoutManager(linearLayoutManager);
+            holder.getSwpFavoritos().setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    atualizaLista();
+                }
+            });
+            atualizaLista();
+        }
+        else {
+            Log.d("FAVORITOSACTIVITY", "favoritos vazio");
+            holder.getSwpFavoritos().setVisibility(View.GONE);
+            holder.getTxtVazio().setVisibility(View.VISIBLE);
+        }
     }
 
     class Holder {
         RecyclerView lstFavoritos;
         FloatingActionButton fab;
         SwipeRefreshLayout swpFavoritos;
+        TextView txtVazio;
 
         Holder() {
             this.lstFavoritos = findViewById(R.id.lstFavoritos);
             this.fab = findViewById(R.id.fab);
             this.swpFavoritos = findViewById(R.id.swpFavoritos);
+            this.txtVazio = findViewById(R.id.txtVazio);
         }
 
         public RecyclerView getLstFavoritos() {
@@ -101,6 +116,89 @@ public class FavoritosActivity extends AppCompatActivity {
 
         public SwipeRefreshLayout getSwpFavoritos() {
             return swpFavoritos;
+        }
+
+        public TextView getTxtVazio() {
+            return txtVazio;
+        }
+    }
+
+    class ClimaAssincrono extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            super.onPostExecute(s);
+            Log.d("FAVORITOSACTIVITY", "MSG = " + s);
+            try
+            {
+                if(!s.isEmpty())
+                {
+                    Gson gson = new Gson();
+                    ListaOpenWeather climas = gson.fromJson(s, ListaOpenWeather.class);
+                    for(int i = 0;i<favoritos.size();i++) {
+                        favoritos.get(i).setClima(climas.getLista().get(i).getClima().get(0).getDescricao());
+                        favoritos.get(i).setTemperatura(climas.getLista().get(i).getMain().getTemperatura());
+                    }
+                    adapter.setFavoritos(favoritos);
+                    holder.getSwpFavoritos().setRefreshing(false);
+                    holder.getLstFavoritos().setVisibility(View.VISIBLE);
+                }
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpsURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try
+            {
+                URL url = new URL(params[0]);
+                Log.d("FAVORITOSACTIVITY", "URL = " + params[0]);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder buffer = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
         }
     }
 }
